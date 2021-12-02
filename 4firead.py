87885474a9980a1d6945ea97cc12a08ae5ed7097
py-46-Alex <90619667+py-46-Alex@users.noreply.github.com>
2021-12-02 16:06:25 +0300

    now all in fuctions
    added all to functions

--- 4firead.py
+++ 4firead.py
@@ -1,33 +1,41 @@
 import os
 from pprint import pprint
 
+def writer():
+    second_dict = listo_creator()
+    collection = start_progr()
+    for name_of_file in second_dict.keys():
+        with open('4fil.txt', 'a') as f:
+            f.write(f'{name_of_file}\n')
+            f.write(f'{second_dict[name_of_file]}\n')
+            f.write(f'{collection[name_of_file]}\n')
+    print("Запись в 4-ый файл в папке готов")
 
-opa ={}
-xxx = {}
-collection = {}
-for files in os.listdir():
-   if files.endswith(('.txt')):
-       with open(files, encoding='utf-8') as data:
-            for strokes in data:
-                collection[files] = data.read()
+def listo_creator():
+    first_dict = {}
+    for files in os.listdir():
+        if files.endswith(('.txt')):
+            counterr = 0
+            with open(files, encoding='utf-8') as data:
+                for strokes in data:
+                    counterr += 1
+                    first_dict[files] = counterr
+    second_dict = {}
+    sorted_vales = sorted(first_dict.values())
+    for w in sorted_vales:
+        for k in first_dict.keys():
+            if first_dict[k] == w:
+                second_dict[k] = first_dict[k]
+   
+    return second_dict
 
-for files in os.listdir():
-    if files.endswith(('.txt')):
-        counterr = 0
-        with open(files, encoding='utf-8') as data:
-            for strokes in data:
-                counterr += 1
-            opa[files] = counterr
+def start_progr(): 
+    collection = {}
+    for files in os.listdir():
+        if files.endswith(('.txt')):
+            with open(files, encoding='utf-8') as data:
+                for strokes in data:
+                    collection[files] = data.read()
+    return collection
 
-sorted_vales = sorted(opa.values())
-
-for w in sorted_vales:
-    for k in opa.keys():
-        if opa[k] == w:
-            xxx[k] = opa[k]
-
-for name_of_file in xxx.keys():
-    with open('4fil.txt', 'a') as f:
-        f.write(f'{name_of_file}\n')
-        f.write(f'{xxx[name_of_file]}\n')
-        f.write(f'{collection[name_of_file]}\n')
+writer()
